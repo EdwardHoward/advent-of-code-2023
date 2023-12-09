188bc86b2e47ff7ddb590b65267f539f59cdb450
EdwardHoward <edwardhoward11@gmail.com>
2023-12-08 22:45:57 -0700

    day 9

---
+++ day-9/part1.js
@@ -0,0 +1,44 @@
+const fs = require('node:fs')
+
+const test = false
+const input = fs.readFileSync(test ? './day-9/test.txt' : './day-9/input.txt').toString()
+
+const lines = input.split('\n')
+
+const sequences = lines.reduce((acc, line) => {
+  let currentVal = line.split(' ').map(Number)
+  const sequenceList = [currentVal]
+
+  while (!currentVal.every(val => val === 0)) {
+    let sequence = []
+
+    for (let i = 0; i < currentVal.length - 1; i++) {
+      if (isNaN(Math.abs(currentVal[i + 1] - currentVal[i]))) debugger
+      sequence.push(currentVal[i + 1] - currentVal[i])
+    }
+
+    if (sequence.length === 0) sequence = [0]
+    currentVal = sequence
+    sequenceList.push(sequence)
+  }
+
+  acc.push(sequenceList)
+
+  return acc
+}, [])
+
+const sum = sequences.reduce((acc, sequence) => {
+  sequence.reverse()
+
+  for (let i = 0; i < sequence.length; i++) {
+    if (i === 0) {
+      sequence.at(i).push(0)
+    } else {
+      sequence.at(i).push(sequence.at(i).at(-1) + sequence.at(i - 1).at(-1))
+    }
+  }
+
+  return acc + sequence.at(-1).at(-1)
+}, 0)
+
+console.log(sum)
